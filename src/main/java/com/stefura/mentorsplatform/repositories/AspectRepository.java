c658b3f97e6d6bb8badee8e9166bc7517647312b
olegSt4 <stefura2010@ukr.net>
2021-02-03 14:13:32 +0200

    refact: redundant methods removed

--- src/main/java/com/stefura/mentorsplatform/repositories/AspectRepository.java
+++ src/main/java/com/stefura/mentorsplatform/repositories/AspectRepository.java
@@ -7,13 +7,10 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface AspectRepository extends CrudRepository<Aspect, Long> {
-    Aspect findByName(String name);
     List<Aspect> findAll();
 
     @Query("SELECT a.name from " +
             "Aspect a JOIN a.profilesWithAspect ap " +
             "WHERE ap.id = ?1")
     List<String> findAspectsNamesByProfileId(Long profileId);
-
-    void deleteByName(String name);
 }
